8f17062dc405b2e98f6e258eb2dd9e3c9425aeed
SushanJit Bhattacherjee <sushanjit123@gmail.com>
2026-09-02 23:07:45 +0530

    Util code addition

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import connectDB from "./db/index.js";
 dotenv.config({path: "./.env"});
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log("Error while connecting to MongoDB", err);
+})
 
 
 
