c1ff8900a26d641cacd68a2fa40375a28d83bc67
SushanJit Bhattacherjee <sushanjit123@gmail.com>
2026-09-02 16:34:29 +0530

    added constants

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
     try{
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/?appName=${DB_NAME}`);
         console.log(`\n MongoDB connected successfully !! DB HOST: ${connectionInstance.connection.host} \n`);
     }
     catch(error){
